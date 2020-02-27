4d5601b0221ec085daedaecbbcdbddb933b50e08
lekhafiblabs <ksrilekha@fiblabs.com>
2020-02-27 11:16:49 +0530

    Association added

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Follow < ApplicationRecord
+  belongs_to :follower, class_name: :User, foreign_key: :following_id
+  belongs_to :following, class_name: :User, foreign_key: :follower_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :tweets
+  has_many :follower_mapping, class_name: :Follow, foreign_key: :follower_id, dependent: :destroy
+  has_many :followers, through: :follower_mapping, source: :follower
+  has_many :following_mapping, class_name: :Follow, foreign_key: :following_id, dependent: :destroy
+  has_many :followings, through: :following_mapping, source: :following
 end
